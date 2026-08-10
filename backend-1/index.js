7acae2e6186b3f968511303b5fcb09a38552ba81
Anand Kumar <anandkumargupta.akg15@gmail.com>
2026-08-10 11:30:50 +0530

    done

--- backend-1/index.js
+++ backend-1/index.js
@@ -2,5 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.use(express);
+app.use(express.json());
+
+app.get('/home', (req,res) =>{
+    res.send("Welcom to homepage")
+    console.log("Welcome to homepage");
+})
+
+
+app.listen(5000, () =>{
+    console.log("Port is running")
+})
 
