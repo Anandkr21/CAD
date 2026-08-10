3afe6b3d4fc84d45fd94b110e4127e44ac86afae
Anand Kumar <anandkumargupta.akg15@gmail.com>
2026-08-10 12:02:12 +0530

    doe

--- backend-1/index.js
+++ backend-1/index.js
@@ -4,14 +4,12 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/home', (req,res) =>{
-    res.send("Welcom to homepage")
-    console.log("Welcome to homepage");
-})
+app.get('/', (req, res) => {
+    res.send('Welcome to homepage');
+});
 
-
-// app.listen(5000, () =>{
-//     console.log("Port is running")
-// })
+app.get('/home', (req, res) => {
+    res.send('Welcome to home route');
+});
 
 module.exports = app;
