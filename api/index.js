e05e7340e6d353289fbf33a1096a4cb247171e0d
Anand Kumar <anandkumargupta.akg15@gmail.com>
2026-08-11 09:49:53 +0530

    initial commit

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Node.js server is running!");
+});
+
+app.get("/users", (req, res) => {
+    res.json([
+        { id: 1, name: "Anand" },
+        { id: 2, name: "Rahul" }
+    ]);
+});
+
+
+module.exports = app;
