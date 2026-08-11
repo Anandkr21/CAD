e27a295b3de481c8dd452c8bf79e9e308120db34
Anand Kumar <anandkumargupta.akg15@gmail.com>
2026-08-11 10:38:55 +0530

    Prepare app for Render

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ app.get("/users", (req, res) => {
     ]);
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
